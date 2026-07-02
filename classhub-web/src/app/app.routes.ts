3768465b58de1c9d38d70cab63113eaf356bd8c7
Daniel Hernandez <daniel244@live.com.mx>
2026-07-01 19:55:15 -0600

    feat(web-navigation): agregar navegación base con vistas placeholder y rutas protegidas con AuthGuard
    - implementación de rutas admin y teacher
    - creación de componentes placeholder (admin, users, teacher, clases)
    - integración de navegación básica desde dashboard
    - corrección de tipado Routes (Angular strict)
    - ajuste de nombres de componentes standalone

--- classhub-web/src/app/app.routes.ts
+++ classhub-web/src/app/app.routes.ts
@@ -1,23 +1,65 @@
+
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
 
+  // LOGIN
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login')
+        .then(m => m.LoginComponent),
+  },
+
+  // DASHBOARD BASE
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/dashboard/dashboard')
+        .then(m => m.DashboardComponent),
+  },
+
+  // ADMIN
+  {
+    path: 'admin',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/admin/admin-dashboard/admin-dashboard')
+        .then(m => m.AdminDashboardComponent),
+  },
+
+  {
+    path: 'admin/users',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/admin/users/users')
+        .then(m => m.UsersComponent),
+  },
+
+  // TEACHER
+  {
+    path: 'teacher',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/teacher/teacher-dashboard/teacher-dashboard')
+        .then(m => m.TeacherDashboardComponent),
+  },
+
+  {
+    path: 'teacher/classes',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/teacher/classes/classes')
+        .then(m => m.ClassesComponent),
+  },
+
+  // DEFAULT
   {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full'
-  },
-
-  {
-    path: 'login',
-    loadComponent: () => import('./features/auth/login/login').then(m => m.LoginComponent),
-  },
-
-  {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    loadComponent: () => import('./features/dashboard/dashboard').then(m => m.DashboardComponent),
-  },
+  }
 
 ];
---
+++ classhub-web/src/app/features/teacher/teacher-dashboard/teacher-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-teacher-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './teacher-dashboard.html',
+  styleUrls: ['./teacher-dashboard.scss'],
+})
+export class TeacherDashboardComponent {}
