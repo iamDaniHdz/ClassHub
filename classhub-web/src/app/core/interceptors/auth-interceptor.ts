36aa66042869ad5bf8d94178615b853c326d50da
Daniel Hernandez <daniel244@live.com.mx>
2026-07-01 18:10:26 -0600

    feat(auth-web): configurar interceptor HTTP y environments para manejo automático de token

--- classhub-web/src/app/core/interceptors/auth-interceptor.ts
+++ classhub-web/src/app/core/interceptors/auth-interceptor.ts
@@ -1,18 +1,37 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
+  const router = inject(Router);
+
   const token = localStorage.getItem('token');
 
-  if (token) {
-    const cloned = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+  let authReq = req;
 
-    return next(cloned);
+  // Agregar token automáticamente
+  if (token) {
+    authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
 
-  return next(req);
+  return next(authReq).pipe(
+    catchError((error) => {
+
+      if (error.status === 401) {
+        console.log('Token expirado');
+
+        localStorage.removeItem('token');
+
+        router.navigateByUrl('/login');
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
